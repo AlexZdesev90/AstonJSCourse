a541d5e978641fc6514827a53ce3826c27c8cdd6
AlexZdesev90 <87057138+AlexZdesev90@users.noreply.github.com>
2023-02-26 23:13:33 +0300

    Errors correction task 1

--- task1.js
+++ task1.js
@@ -1,4 +1,17 @@
 function deepCopyObject(obj) {
+  if (typeof obj !== 'object' 
+  || typeof obj === 'function' 
+  || obj === null) {
+    return obj;
+  }
+
+if (Array.isArray(obj)) {
+        const array = [];
+        obj.forEach((element, i) => {
+            array[i] = deepCopyObject(element);
+        });
+        return array;
+      }
   let copy = {};
   for (let key in obj) {
     if (key instanceof Object) {
