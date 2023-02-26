34f6864e9d9594bb0f98e3a9c0219ddcc8b34ab9
AlexZdesev90 <87057138+AlexZdesev90@users.noreply.github.com>
2023-02-26 22:34:57 +0300

    Errors correction

--- task2.js
+++ task2.js
@@ -1,6 +1,6 @@
 const getNumberRadix = (number, radix) => {
   const throwedErrorMessage = 'Функция getNumberRadix была вызвана с некорректными параметрами';
-  if (radix < 2 || radix > 16 || !Number.isInteger(+number) || !(Number(radix))) {
+  if (radix < 2 || radix > 16 || number < 0 || !Number.isInteger(+number) || !(Number(radix))) {
     throw Error(throwedErrorMessage);
   }
   return (+number).toString(radix);
